2d8ec7534792e123915a9edd3e82d72ce012fe49
Ujjwal Ojha <bring2uo@gmail.com>
2024-05-22 21:56:24 +1000

    Add layer for dynamo db client

---
+++ src/layers.ts
@@ -0,0 +1,47 @@
+import { BaseS3ServiceLayer, S3ClientInstance, S3ClientInstanceConfig, S3ServiceLayer } from "@effect-aws/client-s3";
+import { Layer } from "effect";
+import { MediaContentsRepositoryLive } from "infrastructure/persistence/media-contents.repository.live";
+import { MediaMetadataRepositoryLive } from "infrastructure/persistence/media-metadata.repository.live";
+import { DynamoDBClientInstanceConfig, DynamoDBService, DynamoDBServiceLayer } from "@effect-aws/client-dynamodb";
+
+// DynamoDB Client Configuration
+const DynamoDBClientConfigLayer = Layer.succeed(
+  DynamoDBClientInstanceConfig,
+  {
+    region: process.env.AWS_REGION as string,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+    ...(process.env.AWS_DYNAMODB_ENDPOINT && { endpoint: process.env.AWS_DYNAMODB_ENDPOINT }),
+  },
+);
+const CustomDynamoDBServiceLayer = DynamoDBServiceLayer.pipe(
+  Layer.provide(DynamoDBClientConfigLayer),
+);
+
+// S3 Client Configuration
+const S3ClientConfigLayer = Layer.succeed(
+  S3ClientInstanceConfig,
+  {
+    forcePathStyle: true,
+    region: process.env.AWS_REGION as string,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+    ...(process.env.AWS_S3_ENDPOINT && { endpoint: process.env.AWS_S3_ENDPOINT }),
+  },
+);
+export const CustomS3ServiceLayer = S3ServiceLayer.pipe(
+  Layer.provide(S3ClientConfigLayer),
+);
+
+export default Layer.mergeAll(
+  ...[
+    MediaContentsRepositoryLive,
+    MediaMetadataRepositoryLive,
+    CustomS3ServiceLayer,
+    CustomDynamoDBServiceLayer,
+  ] as const,
+);
