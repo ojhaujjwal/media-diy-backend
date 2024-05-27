a6e9d1f46ae3be16e03a084563a882abbf1c35f0
Ujjwal Ojha <bring2uo@gmail.com>
2024-05-27 22:57:13 +1000

    Make some properties optional in exif metadata

--- src/domain/model/media.ts
+++ src/domain/model/media.ts
@@ -9,11 +9,36 @@ export enum MediaType {
 export const MediaFileExtensionSchema = S.Literal('heic', 'heif', 'jpg', 'jpeg', 'png', 'mp4', 'mov');
 export type MediaFileExtension = S.Schema.Type<typeof MediaFileExtensionSchema>;
 
-export const FILE_EXTENSION_MAPPING: Record<MediaType, ReadonlyArray<MediaFileExtension>> = {
+export const FILE_EXTENSION_MAPPING: Record<MediaType, readonly MediaFileExtension[]> = {
   [MediaType.PHOTO]: ['heic', 'heif', 'jpg', 'jpeg', 'png'],
   [MediaType.LIVE_PHOTO]: ['heic', 'heif'],
   [MediaType.VIDEO]: ['mp4', 'mov', 'heif'],
 } as const;
+
+export const ExifMetadata = S.Struct({
+  width: S.Number,
+  height: S.Number,
+  make: S.String,
+  model: S.String,
+  exposureTime: S.optional(S.String),
+  aperture: S.optional(S.String),
+  focalLength: S.optional(S.String),
+  iso: S.optional(S.Number),
+  orientation: S.optional(S.Number),
+  flash: S.optional(S.Number),
+  whiteBalance: S.optional(S.Number),
+  meteringMode: S.optional(S.Number),
+  exposureMode: S.optional(S.Number),
+  exposureProgram: S.optional(S.Number),
+  exposureBias: S.optional(S.Number),
+  software: S.optional(S.String),
+  gps: S.Struct({
+    latitude: S.Number,
+    longitude: S.Number,
+    altitude: S.Number,
+    timestamp: S.Date,
+  }),
+});
 
 export class MediaMetadata extends S.Class<MediaMetadata>("MediaMetadata")({
   id: S.UUID,
@@ -25,29 +50,5 @@ export class MediaMetadata extends S.Class<MediaMetadata>("MediaMetadata")({
   ownerUserId: S.String,
   uploadedAt: S.Date,
   capturedAt: S.Date,
-  
-  exif: S.optional(S.Struct({
-    width: S.Number,
-    height: S.Number,
-    make: S.String,
-    model: S.String,
-    exposureTime: S.String,
-    aperture: S.String,
-    focalLength: S.String,
-    iso: S.Number,
-    orientation: S.Number,
-    flash: S.Number,
-    whiteBalance: S.Number,
-    meteringMode: S.Number,
-    exposureMode: S.Number,
-    exposureProgram: S.Number,
-    exposureBias: S.Number,
-    software: S.String,
-    gps: S.Struct({
-      latitude: S.Number,
-      longitude: S.Number,
-      altitude: S.Number,
-      timestamp: S.Date,
-    }),
-  })),
+  exif: S.optional(ExifMetadata),
 }) {}
